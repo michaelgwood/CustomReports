e2d95af91239264aa54e56be20343b0d06c6b184
Michael Wood <michael.g.wood@intel.com>
2015-05-20 12:39:55 +0100

    WIP Example add toastertable

--- charts/urls.py
+++ charts/urls.py
@@ -1,4 +1,7 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
+from django.views.generic import TemplateView
+
+import tables
 
 from . import views
 
@@ -7,5 +10,8 @@ urlpatterns = patterns('',
     url(r'^$', 'charts.views.index', name='index'),
     url(r'^dashboard$', 'charts.views.dashboard', name='dashboard'),
     url(r'^testrun/(?P<id>[0-9]+)$', 'charts.views.testrun', name='testrun'),
-    url(r'^testrun/', lambda x: HttpResponseBadRequest(), name='base_testrun'),
+    url(r'^testrun/$', lambda x: HttpResponseBadRequest(), name='base_testrun'),
+    url(r'^tableexample/$', TemplateView.as_view(template_name="charts/exampletablepage.html"), {'table_name': "charts:"+tables.TestPlanTable.__name__.lower()}),
+    url(r'^xhr_tables/', include('charts.tables')),
+
 )
